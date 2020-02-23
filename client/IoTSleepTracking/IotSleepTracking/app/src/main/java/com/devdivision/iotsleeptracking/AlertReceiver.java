8679e50a9cb88e2aae0f62677a8021c16cb5b6e0
Simon <bauchet.simon@gmail.com>
2020-02-23 18:42:15 +0000

    add alarmCLock + sound

--- client/IoTSleepTracking/IotSleepTracking/app/src/main/java/com/devdivision/iotsleeptracking/AlertReceiver.java
+++ client/IoTSleepTracking/IotSleepTracking/app/src/main/java/com/devdivision/iotsleeptracking/AlertReceiver.java
@@ -3,15 +3,25 @@ package com.devdivision.iotsleeptracking;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 
 import androidx.core.app.NotificationCompat;
 
 
 public class AlertReceiver extends BroadcastReceiver {
+    private MediaPlayer mediaPlayer;
     @Override
     public void onReceive(Context context, Intent intent) {
+        mediaPlayer = MediaPlayer.create(context, R.raw.alarm_sound);
         NotificationHelper notificationHelper = new NotificationHelper(context);
         NotificationCompat.Builder nb = notificationHelper.getChannelNotification();
         notificationHelper.getManager().notify(1, nb.build());
+        mediaPlayer.start();
+//        Uri.parse("android.resource://com.devdivision.iotsleeptracking/raw/alarm_sound.mp3")
+    }
+
+    public void stopAlarm() {
+        mediaPlayer.stop();
     }
 }
--- client/IoTSleepTracking/IotSleepTracking/app/src/main/java/com/devdivision/iotsleeptracking/MainActivity.java
+++ client/IoTSleepTracking/IotSleepTracking/app/src/main/java/com/devdivision/iotsleeptracking/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.fragment.app.DialogFragment;
